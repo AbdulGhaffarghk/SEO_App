0b9bbac0800de510332b99617058885f9023a3eb
varoonkumar1 <153481037+varoonkumar1@users.noreply.github.com>
2025-08-09 02:32:46 -0700

    Add files via upload
    main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:seo/screens/CoursesScreen.dart';
 import 'package:seo/screens/DashBoardScreen.dart';
-import 'package:seo/screens/InboxScreen.dart';
-import 'package:seo/screens/NthpScreen.dart';
+import 'package:seo/screens/Dashboard%20Screens/4ButtonScreen.dart';
+import 'package:seo/screens/JobScreen.dart';
+import 'package:seo/screens/Login%20and%20Sign%20up%20Screens/loginScreen.dart';
+import 'package:seo/screens/Login%20and%20Sign%20up%20Screens/onBoardingScreen.dart';
+import 'package:seo/screens/Login%20and%20Sign%20up%20Screens/startingScreen.dart';
+import 'package:seo/screens/Profile%20Screens/freinds.dart';
+import 'package:seo/screens/Profile%20Screens/privacy.dart';
+import 'package:seo/screens/Profile%20Screens/profile.dart';
+import 'package:seo/screens/Profile%20Screens/setting.dart';
 import 'package:seo/screens/ProfileScreen.dart';
 import '../constants.dart';
 
@@ -33,13 +40,21 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
 
-      initialRoute: '/home',
+      initialRoute: '/startingScreen',
       routes: {
-        '/home': (context) => const DashBoardScreen(),
-        '/courses': (context) => const CoursesScreen(),
-        '/inbox': (context) => const InboxScreen(),
+        '/onboarding': (context) => OnboardingScreen(),
+        '/home': (context) => const DashBoardScreen(""),
+        '/courses': (context) => CoursesScreen(),
+        '/jobs': (context) => const Jobscreen(),
         '/profile': (context) => const ProfileScreen(),
-        '/nthp': (context) => const Nthpscreen(),
+        '/startingScreen': (context) => const SplashScreen(),
+        'loginScreen': (context) => const LoginScreen(),
+
+        //Profile Screens
+        '/inviteFriends': (context) => InviteFriendsScreen(),
+        '/privacyPolicy': (context) => PrivacyPolicyScreen(),
+        '/editProfile': (context) => EditProfileScreen(),
+        '/settings': (context) => SettingsScreen(),
       },
       
     );
